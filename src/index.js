db5686daeaa23ffdab26f73eaed622c3420d579b
Ros Haitovich <matrostik@gmail.com>
2018-08-25 22:49:12 +0300

    slds init

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import middlewaresConfig from './config/middlewares';
 import { HomeRoutes } from './controllers';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 /**
  * MIDDLEWARES
@@ -16,15 +16,16 @@ middlewaresConfig(app);
 /**
  * VIEW ENGINE
  */
-
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 /**
  * ROUTES
  */
-
 app.use('/', [HomeRoutes]);
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "static")));
 
 app.listen(PORT, (err) => {
     if (err) {
