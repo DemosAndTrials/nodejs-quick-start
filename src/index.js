0fd2992a803d051bff7a744375f5364e7baa5304
Ros Haitovich <matrostik@gmail.com>
2018-08-31 23:11:14 +0300

    custom error page

--- src/index.js
+++ src/index.js
@@ -27,11 +27,33 @@ app.use('/', [HomeRoutes]);
 // Serve static files
 app.use(express.static(path.join(__dirname, "static")));
 
+// $ curl http://localhost:3000/notfound
+// $ curl http://localhost:3000/notfound -H "Accept: application/json"
+// $ curl http://localhost:3000/notfound -H "Accept: text/plain"
+app.use(function (req, res, next) {
+    res.status(404);
+    
+    res.format({
+        html: function () {
+            res.render('pages/404', {
+                url: req.url
+            })
+        },
+        json: function () {
+            res.json({
+                error: 'Not found'
+            })
+        },
+        default: function () {
+            res.type('txt').send('Not found')
+        }
+    })
+});
+
 app.listen(PORT, (err) => {
     if (err) {
         console.error(err);
-    }
-    else {
+    } else {
         console.log(`Server Running on port: ${PORT}`);
     }
 });
